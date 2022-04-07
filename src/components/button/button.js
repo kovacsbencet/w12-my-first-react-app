b28201083c9b215466f79cd53b98ae4591fbaa48
kovacsbencet <kovacsbencet@gmail.com>
2022-04-07 18:25:10 +0200

    My first react application

---
+++ src/components/button/button.js
@@ -0,0 +1,15 @@
+
+const text = "First React App"
+
+//Először Javascript objektumot generál le, majd abból készít sima HTML kódot
+function Button() {
+  return (
+    <>
+        <div>
+            <button>Button</button>
+        </div>
+    </>
+  );
+}
+
+export default Button;
