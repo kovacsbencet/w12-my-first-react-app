e61c7a21f533e4a756a3a6203db2f5f434b72c95
kovacsbencet <kovacsbencet@gmail.com>
2022-04-08 14:51:25 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import App2321 from './App2';
+import Beers from './components/Beers'
+import data from './data'
 
+console.log(data)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <App2321 />
+    <Beers />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
