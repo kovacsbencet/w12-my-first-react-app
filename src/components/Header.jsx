5adc2610e9c40b224a07427474ea84bfefc35dbe
kovacsbencet <kovacsbencet@gmail.com>
2022-04-11 16:47:56 +0200

    .

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import {Component} from "react"
+
+class Header extends Component{
+    render(){
+        return (
+            <header>
+                {this.props.title}
+            </header>
+        )
+    }
+}
+export default Header
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Beers from './components/Beers'
 import data from './data'
+import Header from './components/Header'
 
 //console.log(data.logo)
 //console.log(data.cards)
 //console.log(<Beers list={data.cards} />);
 ReactDOM.render(
   <React.StrictMode>
+    <Header title={data.logo}/>
     <Beers list={data.cards} />
   </React.StrictMode>,
   document.getElementById('root')
